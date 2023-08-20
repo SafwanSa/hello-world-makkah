d556314e2b9ee2ba03811405acb36f7dc652c71f
SafwanSa <safwan9f@gmail.com>
2023-08-20 22:49:35 +0300

    install the proper version of firebase and fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'hello-world-makkah',
+    appId: '1:599481463922:web:b7db2df76945d95c123eeb',
+    storageBucket: 'hello-world-makkah.appspot.com',
+    apiKey: 'AIzaSyDk0q7NK9iVRdLwTah15yHMw8YAyFMMSVI',
+    authDomain: 'hello-world-makkah.firebaseapp.com',
+    messagingSenderId: '599481463922',
+  },
   production: true
 };
